663f9860fc08787a381720b63aaeac4f82ba0f4d
chalana kalpitha <chalanakalpitha72@gmail.com>
2024-02-18 22:46:55 +0530

    dashoboard divide

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import { Col, Row } from "antd";
+import React from "react";
 
 export const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
-
+    <div>
+      <Row
+        gutter={[32, 32]}
+        style={{
+          marginTop: "32px",
+        }}
+      >
+        <Col
+          xs={24}
+          sm={24}
+          xl={8}
+          style={{
+            height: "460px",
+          }}
+        >
+          Calendar UpComming Events
+        </Col>
+        <Col
+          xs={24}
+          sm={24}
+          xl={8}
+          style={{
+            height: "460px",
+          }}
+        >
+          Dashboard Deal Chart
+        </Col>
+      </Row>
+    </div>
+  );
+};
