810acd273246b94dad78bebfbb1b90688eef96bd
chalana kalpitha <chalanakalpitha72@gmail.com>
2024-02-18 22:32:49 +0530

    add routing

---
+++ src/config/resources.tsx
@@ -0,0 +1,14 @@
+import { DashboardOutlined } from "@ant-design/icons";
+import { IResourceItem } from "@refinedev/core";
+
+
+export const resources: IResourceItem[] = [
+    {
+        name: 'dashboard',
+        list: '/',
+        meta: {
+            label: 'Dashboard',
+            icon: <DashboardOutlined />
+        }
+    }
+]
