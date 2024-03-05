abcc9f78b518fc8ba33b90e3c2561b923bcdff3b
chalana kalpitha <chalanakalpitha72@gmail.com>
2024-03-05 21:46:31 +0530

    add antd graph

--- src/components/home/deals-chart.tsx
+++ src/components/home/deals-chart.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import { DollarOutlined } from "@ant-design/icons";
+import { Card } from "antd";
+import React from "react";
+import { Text } from "../text";
+import { Area, AreaConfig } from "@ant-design/plots";
 
 const DealsChart = () => {
-  return (
-    <div>DealsChart</div>
-  )
-}
 
-export default DealsChart
+  const config: AreaConfig = {
+    data : []
+  }
+
+  return (
+    <div>
+      <Card
+        style={{ height: "100%" }}
+        headStyle={{ padding: "8px 16px" }}
+        bodyStyle={{ padding: "24px 24px 0 24px" }}
+        title={
+          <div style={{ display: "flex", alignItems: "center", gap: "8px" }}>
+            <DollarOutlined />
+            <Text size="sm" style={{ marginLeft: "0.5rem" }}>
+              Deals
+            </Text>
+          </div>
+        }
+      >
+        <Area {...config} height={325} />
+      </Card>
+    </div>
+  );
+};
+
+export default DealsChart;
